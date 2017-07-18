6045b5c557e486be4576e3e2e92a8a9ddd5f5a24
dinhngochien <hiendinhngoc@yahoo.com.vn>
2017-07-18 09:51:48 +0700

    Add devise Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :received_messages, class_name: 'Message', foreign_key: :user_to_id
   has_many :sent_messages, class_name: 'Message', foreign_key: :user_from_id
